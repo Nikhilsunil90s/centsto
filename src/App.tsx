3e948b5c9be4c8ea5a44563018b519edc4828a12
Nischay Chandra <chandranischay@gmail.com>
2022-04-21 02:32:42 +0530

    lending solutions page, tabs remaining

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 
 import Footer from './components/Footer';
 import About from './components/About';
+import Solutions from './components/Solutions';
 
 function App() {
   return (
     <div className="App">
-      <About/>
+      {/* <About/> */}
+      <Solutions/>
       <Footer/>
     </div>
   );
